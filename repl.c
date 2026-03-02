ee61057f04d76feff73a3d71d50fff221e9456d2
Joao-ava <ajoao781238@gmail.com>
2026-03-01 22:55:02 -0300

    change: separate repl logic in other file

---
+++ repl.c
@@ -0,0 +1,60 @@
+#include "repl.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+InputBuffer* new_input_buffer() {
+    InputBuffer* input_buffer = (InputBuffer*) malloc(sizeof(InputBuffer));
+    input_buffer->buffer = NULL;
+    input_buffer->buffer_length = 0;
+    input_buffer->input_length = 0;
+
+    return input_buffer;
+}
+
+void print_prompt() {
+    printf("db > ");
+}
+
+ssize_t portable_getline(char **lineptr, size_t *n, FILE *stream) {
+    if (*lineptr == NULL || *n == 0) {
+        *n = 128;
+        *lineptr = malloc(*n);
+        if (*lineptr == NULL) return -1;
+    }
+
+    size_t pos = 0;
+    int c;
+
+    while ((c = fgetc(stream)) != EOF) {
+        if (pos + 1 >= *n) {
+            *n *= 2;
+            char *new_ptr = realloc(*lineptr, *n);
+            if (!new_ptr) return -1;
+
+            *lineptr = new_ptr;
+        }
+        (*lineptr)[pos++] = (char) c;
+        if (c == '\n') break;
+    }
+    if (pos == 0 && c == EOF) return -1;
+
+    (*lineptr)[pos] = '\0';
+    return pos;
+}
+
+void read_input(InputBuffer* input_buffer) {
+    ssize_t bytes_read = portable_getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
+    if (bytes_read <= 0) {
+        printf("Error reading input\n");
+        exit(EXIT_FAILURE);
+    }
+
+    input_buffer->input_length = bytes_read - 1;
+    input_buffer->buffer[bytes_read - 1] = 0;
+}
+
+void close_input_buffer(InputBuffer* input_buffer) {
+    free(input_buffer->buffer);
+    free(input_buffer);
+}
---
+++ repl.h
@@ -0,0 +1,15 @@
+#ifndef SIMPLE_DB_REPL_H
+#define SIMPLE_DB_REPL_H
+#include <stddef.h>
+
+typedef struct {
+    char* buffer;
+    size_t buffer_length;
+    ssize_t input_length;
+} InputBuffer;
+
+InputBuffer* new_input_buffer();
+void print_prompt();
+void read_input(InputBuffer* input_buffer);
+void close_input_buffer(InputBuffer* input_buffer);
+#endif //SIMPLE_DB_REPL_H
